5ffd9750952094b44f7e782ef73e307c6297caea
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2023-11-28 09:53:35 +0100

    Implemented splash screen with fade out effect

--- app/src/main/java/com/example/cocktails9/MainActivity.kt
+++ app/src/main/java/com/example/cocktails9/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.cocktails9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().setOnExitAnimationListener {
+                splashScreenView ->
+            splashScreenView.iconView
+                .animate()
+                .setDuration(
+                    3000L
+                )
+                .alpha(0f)
+                .withEndAction {
+                    splashScreenView.remove()
+                }.start()
+        }
         setContentView(R.layout.activity_main)
     }
 }
