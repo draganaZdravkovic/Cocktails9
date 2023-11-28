1190b9f8ac7d2d5782bf52b02f3fef2f1845524e
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2023-11-28 10:51:02 +0100

    Changed splash effect from fade out to fade in

--- app/src/main/java/com/example/cocktails9/MainActivity.kt
+++ app/src/main/java/com/example/cocktails9/MainActivity.kt
@@ -1,25 +1,24 @@
 package com.example.cocktails9
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    private var keepSplashOnScreen = true
+    private val delay = 2000L
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        installSplashScreen().setOnExitAnimationListener {
-                splashScreenView ->
-            splashScreenView.iconView
-                .animate()
-                .setDuration(
-                    3000L
-                )
-                .alpha(0f)
-                .withEndAction {
-                    splashScreenView.remove()
-                }.start()
-        }
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
+
         setContentView(R.layout.activity_main)
     }
 }
