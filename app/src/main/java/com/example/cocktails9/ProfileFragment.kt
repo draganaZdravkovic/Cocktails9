09e8a055a3eb1311eabcc9377c282391436146f0
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2023-11-29 13:19:39 +0100

    Implemented design for profile fragment.

--- app/src/main/java/com/example/cocktails9/ProfileFragment.kt
+++ app/src/main/java/com/example/cocktails9/ProfileFragment.kt
@@ -1,5 +1,19 @@
 package com.example.cocktails9
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class ProfileFragment : Fragment(R.layout.fragment_profile)
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btLogout = view.findViewById<Button>(R.id.btLogout)
+        btLogout.setOnClickListener {
+            Toast.makeText(requireContext(), "Trying to escape?", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
