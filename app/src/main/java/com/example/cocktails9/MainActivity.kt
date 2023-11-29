9e943e89d26658dcecc28866f3da2b9ad92c6f84
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2023-11-29 10:31:35 +0100

    Code refactor.

--- app/src/main/java/com/example/cocktails9/MainActivity.kt
+++ app/src/main/java/com/example/cocktails9/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.cocktails9
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import android.os.Handler
-import android.os.Looper
-import androidx.annotation.RequiresApi
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     private var keepSplashOnScreen = true
@@ -28,7 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragmentContainerView)
-        val appBarConfig = AppBarConfiguration(setOf(R.id.cocktailsFragment, R.id.favoritesFragment, R.id.profileFragment))
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.cocktailsFragment,
+                R.id.favoritesFragment,
+                R.id.profileFragment
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfig)
 
         bottomNavigationView.setupWithNavController(navController)
