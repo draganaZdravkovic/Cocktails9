92afbb7e514d9db4ee6d19ff721155c1dd6a91f0
draganaZdravkovic <dragana.zdravkovic602@gmail.com>
2023-12-01 11:24:47 +0100

    Fixed button in profile fragment. Removed reference to RV and used it from binding. Extracted custom color for toolbar.

--- app/src/main/java/com/example/cocktails9/CocktailsFragment.kt
+++ app/src/main/java/com/example/cocktails9/CocktailsFragment.kt
@@ -6,13 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.cocktails9.databinding.FragmentCocktailsBinding
 
 class CocktailsFragment : Fragment(R.layout.fragment_cocktails) {
     private var _binding: FragmentCocktailsBinding? = null
     private val binding get() = _binding!!
-    private lateinit var recyclerView: RecyclerView
     private lateinit var cocktailsList: MutableList<Cocktails>
     private lateinit var cocktailsAdapter: CocktailsAdapter
 
@@ -37,13 +35,12 @@ class CocktailsFragment : Fragment(R.layout.fragment_cocktails) {
     }
 
     private fun initRecyclerView() {
-        recyclerView = binding.rvCocktails
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.rvCocktails.layoutManager = GridLayoutManager(requireContext(), 2)
 
         cocktailsList = addDataToList()
 
         cocktailsAdapter = CocktailsAdapter(cocktailsList)
-        recyclerView.adapter = cocktailsAdapter
+        binding.rvCocktails.adapter = cocktailsAdapter
     }
 
     private fun addDataToList(): MutableList<Cocktails> {
